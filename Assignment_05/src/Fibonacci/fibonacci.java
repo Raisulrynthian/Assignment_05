c16ca010608701bb6c6c13b6d7175060ac4a383f
Raisulrynthian <raisul05@student.sust.edu>
2020-03-01 23:40:00 +0600

    .

--- Assignment_05/src/Fibonacci/fibonacci.java
+++ Assignment_05/src/Fibonacci/fibonacci.java
@@ -4,12 +4,13 @@ public class fibonacci {
 
     public void fibonacci(int number) {
         int a = 0, b = 1, c = 0;
+        System.out.print(a + " "+ b +" ");
         while (number >= 1) {
-            System.out.print(c + " ");
             c = a + b;
             a = b;
             b = c;
-            number-=1;
+            System.out.print(c + " ");
+            number -= 1;
         }
     }
 }
